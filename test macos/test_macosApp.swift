0da948bed6bb4b4e3e2172baae100b2f417be647
Chris Athanas <realityexpander@gmail.com>
2023-03-09 01:59:00 -0600

    Initial Commit

---
+++ test macos/test_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_macosApp.swift
+//  test macos
+//
+//  Created by Chris Athanas on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct test_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
