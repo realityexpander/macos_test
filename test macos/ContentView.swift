cee9c01e4135dbe8ad5c27afbc575db6871fbf03
Chris Athanas <realityexpander@gmail.com>
2023-03-09 02:48:01 -0600

    initial commit

--- test macos/ContentView.swift
+++ test macos/ContentView.swift
@@ -6,14 +6,44 @@
 //
 
 import SwiftUI
+import Cocoa
 
 struct ContentView: View {
+    @State var count = 1
+    
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world! " + count.formatted())
+            
+            Button(action: {
+                print("heellooo")
+                count = count + 1
+            }, label: {
+                Text("click")
+            })
+            
+            Button(action: {
+                print("Slapping")
+
+                var str = "Hello, playground"
+
+                // get URL to the the documents directory in the sandbox
+                let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as NSURL
+
+                // add a filename
+                let fileUrl = documentsUrl.appendingPathComponent("foo.txt")
+
+                // write to it
+                try! str.write(to: fileUrl!, atomically: true, encoding: String.Encoding.utf8)
+                
+                
+            }, label: {
+                Text("click")
+            })
         }
         .padding()
     }
